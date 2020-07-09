6df4a7afcb770343d00209d7c427f48d19cb09b5
natoine <antoineseilles@gmail.com>
2020-07-09 19:52:24 +0200

    now folder public is public

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index')
