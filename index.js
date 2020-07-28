6aee1e4fce6674162fa8a41ae36079427673686c
natoine <antoineseilles@gmail.com>
2020-07-28 19:43:24 +0200

    it works get data from index form but bodyparser() deprecated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
+var bodyParser = require('body-parser')
+app.use(bodyParser())
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -14,6 +16,21 @@ app.get('/', function (req, res) {
 app.get('/main/:id', function (req, res) {
     var id = req.params.id ;
     res.render('main', {id: id})
+})
+
+//receives index form with patient data
+//redirects to the first hand picture test
+app.post('/main', function (req, res) {
+  console.log("post body", req.body)
+  var user = {};
+  user.codepatient = req.body.codepatient ;
+  user.age = req.body.age ;
+  user.sex = req.body.sex ;
+  user.lat = req.body.lat ;
+  user.csp1 = req.body.csp1 ;
+  user.csp2 = req.body.csp2 ;
+
+  console.log("user", user)
 })
 
 app.listen(port, function () {
