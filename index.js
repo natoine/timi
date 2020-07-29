2523b013a06d253a58c2eedc6dd40feff9b658f4
natoine <antoineseilles@gmail.com>
2020-07-29 16:00:22 +0200

    cannot go randomly to a question

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/main/:id', function (req, res) {
-    if(!req.session.user) res.redirect(307,'/')
-    else {
-      let id = req.params.id ;
-      //console.log("user session", req.session.user)
-      res.render('main', {id: id})
+    if(!req.session.user) res.redirect(307,'/') //cannot start test without a user
+    else { //verifies coherency
+      let id = parseInt(req.params.id) ;
+      let answerslength = req.session.user.answers.length ;
+      if(answerslength === 0 && id === 1) res.render('main', {id: id});
+      else
+      {
+        if(answerslength === 0 ) res.redirect(307, '/main/1');
+        else {
+          let latestdiapo = parseInt( req.session.user.answers[answerslength - 1].diapo ) ;
+          if(latestdiapo + 1 === id) res.render('main', {id: id});
+          else {
+            let goodid = latestdiapo + 1;
+            res.redirect(307, '/main/' + goodid);
+          }
+        }
+      }
     }
 })
 
@@ -36,7 +48,7 @@ app.post('/main', function (req, res) {
   let user = req.body.user ;
   req.session.user = user ;
   req.session.user.answers = [];
-  res.redirect(301, '/main/1')
+  res.redirect(301, '/main/1');
 })
 
 //receives answer
@@ -44,11 +56,11 @@ app.post('/main', function (req, res) {
 app.post('/answer', function (req, res) {
   //console.log("post body", req.body)
   let answer = req.body ;
-  req.session.user.answers.push(answer)
-  console.log("user", req.session.user)
+  req.session.user.answers.push(answer);
+  console.log("user", req.session.user);
   let next = parseInt(answer.diapo) + 1;
-  if(next <= 48) res.redirect(301, '/main/'+next)
-  else res.redirect(301, '/score')
+  if(next <= 48) res.redirect(301, '/main/'+next);
+  else res.redirect(301, '/score');
 })
 
 app.listen(port, function () {
