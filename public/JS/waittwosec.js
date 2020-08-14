217125576f9714d3367f70974340385faddafbb5
natoine <antoineseilles@gmail.com>
2020-08-14 17:18:00 +0200

    should be better for url bar

--- public/JS/waittwosec.js
+++ public/JS/waittwosec.js
@@ -5,12 +5,16 @@ var delay = 2000 ;
 function switchvisibility(){
     document.getElementById("waitcontent").style.visibility = "hidden" ;
     document.getElementById("maincontent").style.visibility = "visible" ;
-    window.onload=function(){ setTimeout(function(){ 		
-        console.log("scroll");
-        window.scrollTo(0, 1); 	}, 0); }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 
     setTimeout(switchvisibility, delay);
 })
+
+window.onload=function(){ 
+    setTimeout(function(){ 		
+        console.log("scroll");
+        window.scrollTo(0, 1); 	
+    }, delay); 
+}
