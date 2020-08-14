17a4d7385a96e7ecf1ca732b16092cae077a1eac
natoine <antoineseilles@gmail.com>
2020-08-14 17:12:22 +0200

    try something for url bar on mobile

--- public/JS/waittwosec.js
+++ public/JS/waittwosec.js
@@ -5,6 +5,9 @@ var delay = 2000 ;
 function switchvisibility(){
     document.getElementById("waitcontent").style.visibility = "hidden" ;
     document.getElementById("maincontent").style.visibility = "visible" ;
+    window.onload=function(){ setTimeout(function(){ 		
+        console.log("scroll");
+        window.scrollTo(0, 1); 	}, 0); }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
